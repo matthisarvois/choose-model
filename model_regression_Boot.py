da0af492d65b17665556b22036941e9928677cd6
matthisarvois <arvois.matthis60@gmail.com>
2025-12-03 18:13:09 +0100

    ajout de la méthode Rige et optimisation des paramètres

--- model_regression_Boot.py
+++ model_regression_Boot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn import datasets
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 
